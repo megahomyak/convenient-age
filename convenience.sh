3b1172a10c5acd97a390ef7a5fca96ad8e8d1c78
megahomyak <g.megahomyak@gmail.com>
2025-06-18 19:48:12 +0500

    Tried to go with a "personal" directory, decided not to

--- convenience.sh
+++ convenience.sh
@@ -1,23 +1,52 @@
 #!/bin/bash
 set -euo pipefail
-cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
 ARG="$1"
 shift
-if [ "$ARG" == "gen" ]; then
-    if [ -f priv ] || [ -f pub ]; then
-        echo "personal keys already exist" >&2
+if [ "$ARG" == "setup" ]; then
+    mkdir -p "$CONVENIENT_AGE_STORAGE_DIRECTORY"
+    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
+    if [ -d personal ]; then
+        echo "already set up" >&2
     else
-        (age-keygen 2>pub) | age -p >priv
+        mkdir personal
+        cd personal
+        read -p "Paste private key: " PRIVATE_KEY
+        echo "$PRIVATE_KEY" | age -p > priv
+        read -p "Paste public key: " PUBLIC_KEY
+        echo "$PUBLIC_KEY" > pub
     fi
 elif [ "$ARG" == "enc" ]; then
-    RECIPIENT_KEY_NAME="$1"
+    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
+    RECIPIENT_PUBLIC_KEY_FILE_NAME="$1"
     shift
     "$EDITOR" temp
-    cat temp | age -e -a -R "recipients/$RECIPIENT_KEY_NAME"
+    cat temp | age -e -a -R "recipients/$RECIPIENT_PUBLIC_KEY_FILE_NAME"
     rm temp
 elif [ "$ARG" == "dec" ]; then
+    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
     TEMP="$(mktemp)"
     "$EDITOR" temp
-    age -d -i <(cat priv | age -d) temp
+    age -d -i <(cat personal/priv | age -d) temp
     rm temp
+elif [ "$ARG" == "list" ]; then
+    ls "$CONVENIENT_AGE_STORAGE_DIRECTORY/recipients"
+elif [ "$ARG" == "me" ]; then
+    cat "$CONVENIENT_AGE_STORAGE_DIRECTORY/personal/pub"
+elif [ "$ARG" == "add" ]; then
+    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
+    RECIPIENT_PUBLIC_KEY_FILE_NAME="$1"
+    shift
+    RECIPIENT_PUBLIC_KEY="$1"
+    shift
+    mkdir -p recipients
+    cd recipients
+    echo "$RECIPIENT_PUBLIC_KEY" > "$RECIPIENT_PUBLIC_KEY_FILE_NAME"
+elif [ "$ARG" == "rm" ]; then
+    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY/recipients"
+    RECIPIENT_PUBLIC_KEY_FILE_NAME="$1"
+    shift
+    echo "Deleting $(cat "$(echo "$RECIPIENT_PUBLIC_KEY_FILE_NAME")")"
+    rm "$RECIPIENT_PUBLIC_KEY_FILE_NAME"
+else
+    echo "unknown command" >&2
 fi
