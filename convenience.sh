55e3edf1d3b60cd1d64e5a35f37622284bf3e3fb
megahomyak <g.megahomyak@gmail.com>
2025-06-18 20:17:25 +0500

    Added a lot of quality-of-life-improving commands

--- convenience.sh
+++ convenience.sh
@@ -3,17 +3,19 @@ set -euo pipefail
 ARG="$1"
 shift
 if [ "$ARG" == "setup" ]; then
-    mkdir -p "$CONVENIENT_AGE_STORAGE_DIRECTORY"
+    mkdir -p "$CONVENIENT_AGE_STORAGE_DIRECTORY/recipients"
     cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
     if [ -d personal ]; then
         echo "already set up" >&2
     else
-        mkdir personal
-        cd personal
+        mkdir -p temp_personal
+        cd temp_personal
         read -p "Paste private key: " PRIVATE_KEY
         echo "$PRIVATE_KEY" | age -p > priv
         read -p "Paste public key: " PUBLIC_KEY
         echo "$PUBLIC_KEY" > pub
+        cd ..
+        mv temp_personal personal
     fi
 elif [ "$ARG" == "enc" ]; then
     cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
@@ -28,24 +30,23 @@ elif [ "$ARG" == "dec" ]; then
     "$EDITOR" temp
     age -d -i <(cat personal/priv | age -d) temp
     rm temp
-elif [ "$ARG" == "list" ]; then
+elif [ "$ARG" == "ls" ]; then
     ls "$CONVENIENT_AGE_STORAGE_DIRECTORY/recipients"
 elif [ "$ARG" == "me" ]; then
     cat "$CONVENIENT_AGE_STORAGE_DIRECTORY/personal/pub"
 elif [ "$ARG" == "add" ]; then
-    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY"
+    cd "$CONVENIENT_AGE_STORAGE_DIRECTORY/recipients"
     RECIPIENT_PUBLIC_KEY_FILE_NAME="$1"
     shift
     RECIPIENT_PUBLIC_KEY="$1"
     shift
-    mkdir -p recipients
-    cd recipients
     echo "$RECIPIENT_PUBLIC_KEY" > "$RECIPIENT_PUBLIC_KEY_FILE_NAME"
 elif [ "$ARG" == "rm" ]; then
     cd "$CONVENIENT_AGE_STORAGE_DIRECTORY/recipients"
     RECIPIENT_PUBLIC_KEY_FILE_NAME="$1"
     shift
-    echo "Deleting $(cat "$(echo "$RECIPIENT_PUBLIC_KEY_FILE_NAME")")"
+    RECIPIENT_PUBLIC_KEY="$(cat "$RECIPIENT_PUBLIC_KEY_FILE_NAME")"
+    echo "Deleting $RECIPIENT_PUBLIC_KEY"
     rm "$RECIPIENT_PUBLIC_KEY_FILE_NAME"
 else
     echo "unknown command" >&2
